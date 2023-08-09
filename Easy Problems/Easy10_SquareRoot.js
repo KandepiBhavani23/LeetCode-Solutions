b73ae415b24f59811fe7c1bab1ef38ff29de2954
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-08-09 16:05:57 +0530

    SquareRoot.js

--- Easy Problems/Easy10_SquareRoot.js
+++ Easy Problems/Easy10_SquareRoot.js
@@ -1,5 +1,4 @@
 /*
-
 69. Sqrt(x)
 
 Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
@@ -18,7 +17,6 @@ Example 2:
 Input: x = 8
 Output: 2
 Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
-
 */
 const x = 7;
 var mySqrt = function (x) {
