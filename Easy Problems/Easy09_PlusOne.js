432b3d259fd3a79dedd6dcfe219710ac59769673
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-08-09 16:06:20 +0530

    PlusOne.js

--- Easy Problems/Easy09_PlusOne.js
+++ Easy Problems/Easy09_PlusOne.js
@@ -1,14 +1,11 @@
 // https://leetcode.com/problems/plus-one/
 
 /*
-
 66. Plus One
 
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
 Increment the large integer by one and return the resulting array of digits.
-
- 
 
 Example 1:
 Input: digits = [1,2,3]
@@ -32,8 +29,8 @@ Output: [1,0]
 Explanation: The array represents the integer 9.
 Incrementing by one gives 9 + 1 = 10.
 Thus, the result should be [1,0].
-
 */
+
 const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
 var plusOne = function (digits) {
 	const join = BigInt(digits.join("")) + BigInt(1);
